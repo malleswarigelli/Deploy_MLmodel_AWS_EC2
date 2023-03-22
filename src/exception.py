9012dd7c982f8e95878a09c27e8c4ee591740faf
Malli Gelli <malleswari.gelli@gmail.com>
2023-03-22 10:37:39 -0500

    logging and exception

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,7 @@
 
 
 import sys
-from src.logger import logging
+from logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -21,3 +21,12 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
+    
+# # for checking if exception.py working in command prompt
+# if __name__=="__main__":
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info("Divide by Zero")
+#         raise CustomException(e, sys)
+        
